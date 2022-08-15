54d6ad4610248b1d5e9b35cfb9668bb52c5ad205
syarikim <omojitex789@gmail.com>
2022-08-15 22:29:05 +0900

    postモデル追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  root to: 'samples#index'
+  # root to: 'samples#index'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
   get '/login', to: 'user_sessions#new'
   post '/login', to: 'user_sessions#create'
   delete '/logout', to: 'user_sessions#destroy'
+  root to: 'posts#index'
+  resources :posts
 end
